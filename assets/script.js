ba50e1046e3b9b716659bed33433e53195d1ba7b
oscarcrackdev <bauposcar@gmail.com>
2025-07-23 09:33:53 +0200

    first-commit

---
+++ assets/script.js
@@ -0,0 +1,98 @@
+ alert('1');
+ let toto = 'toto';
+
+ function firstFunction(/*paramétrer */) {
+    // son code
+    alert('2');
+
+ }
+
+ alert();
+
+let integer = 10;
+let float = 1.5;
+let string = 'string';
+  let transformerenmaj = "nous sommes l'avenir du dev";
+  console.log('une phrase'.toUpperCase());;
+   
+  
+  let txt = "nous sommes l'avenir du dev";
+   console.log(txt);
+   let txtMaj = txt.toUpperCase();
+   console.log(txtMaj);
+
+   //avec une function mettre en minuscule 
+   //avec une function enlever les espaces
+  // avec une function verifier si le mot nous est contenu dans la sting 
+ //  avec une function compter le nombre de characteres d'une string 
+      
+  
+  
+   
+   let texte = "nous sommes l'avenir du dev !";
+   let texteEnMinuscule = texte.toLowerCase();
+   console.log(texteEnMinuscule);
+   
+   
+
+   let txtSpace = texteEnMinuscule.replaceAll(" ","");
+   console.log(txtSpace);
+    
+   
+
+   let txtLenght = txtSpace.length;
+   console.log(txtLenght);
+   
+   
+
+   let txtInclude = txt.includes("nous");
+   console.log(txtInclude);
+
+   
+
+   let random = Math.random()*9 + 1;
+   console.log(random);
+
+   let round = (Math.round(random));
+   console.log(round);
+
+   let a = 10;
+   let b = 15;
+   let c = 250;
+   let d = -100
+   let total = 10 + 17 + 250 - 100;
+   console.log(total);
+
+   
+
+   
+
+
+     
+
+
+
+   
+   
+
+  
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+   
+   
+  
+  
+  
